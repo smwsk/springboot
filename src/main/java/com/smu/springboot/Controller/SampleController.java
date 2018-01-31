7120876d3eb640a8264b9700ba7c78053bb6b9ac
smwsk <357531851@qq.com>
2018-01-31 16:05:49 +0800

    springboot入门

---
+++ src/main/java/com/smu/springboot/Controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.smu.springboot.Controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+	@ResponseBody
+	@RequestMapping(value="/")
+	public String home(){
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SampleController.class, args);
+	}
+}
